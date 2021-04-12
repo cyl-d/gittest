10b6a28e7ab88b428253cac63e93540aefc39295
cyl <lin0007@foxmail.com>
2021-04-12 18:43:01 +0800

    cyl-d test 提交第一次

--- src/main/java/com/atguigu/gittest/Hello.java
+++ src/main/java/com/atguigu/gittest/Hello.java
@@ -9,6 +9,8 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
+        System.out.println("cyl-d改了");
+        System.out.println("cyl-d test");
     }
 
 }
